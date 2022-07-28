26048239d49620fc64c523f26096a4136afc9609
Ney Roman <romanher_21@hotmail.com>
2022-07-27 22:09:43 -0500

    algo aa

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -1,18 +1,7 @@
 const handlers = ({ axios }) => ({
     getJokes: async (req, res) => {
-        let idJokes = [];
-        let jokes = []
-
         try{
-            for(i=0; idJokes.length<15; i++){
-                let result = await axios.get('https://api.chucknorris.io/jokes/random')
-                if(!(idJokes.indexOf(result.data.id) != -1)){
-                    jokes.push(result.data)
-                    idJokes.push(result.data.id)
-                }
-            }
-
-            res.status(200).json(jokes)
+            res.status(200).json({jokes: 'lapmt'});
 
         }catch(err){
             res.status(500).send(`An error has ocurred ${err}` )
